317d996c6385385bdb14e3d0f53e1b683fad168b
Jonah Greenthal <jonah@jonahgreenthal.com>
2019-06-10 07:28:01 -0500

    Separate question-related types into a new page

--- src/components/header.js
+++ src/components/header.js
@@ -27,6 +27,9 @@ const Header = ({ siteTitle }) => (
           <Link className="page-link" to="/match">
             Match
           </Link>
+          <Link className="page-link" to="/question">
+            Question
+          </Link>
           <Link className="page-link" to="/team">
             Team
           </Link>
